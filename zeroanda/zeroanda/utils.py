ae7162790a9daa1c02525f53eafe905b3a56b6bc
wada <wada@strippers.jp>
2016-04-15 12:01:24 +0900

    add footer to change_list.

--- zeroanda/zeroanda/utils.py
+++ zeroanda/zeroanda/utils.py
@@ -27,22 +27,57 @@ def get_ask_target_point(reference_value):
     return math.floor((reference_value + settings.IFDOCO_ASK_ENTRY_POINT) * 1000) / 1000
 
 def get_ask_upper_bound(reference_value):
-    return math.floor((reference_value + settings.ASK_UPPER_BOUND_PROFIT_MARGIN) * 1000) / 1000
+    utils.info("get_ask_upper_bound")
+    utils.info(reference_value)
+    target = math.floor((reference_value + settings.ASK_UPPER_BOUND_PROFIT_MARGIN) * 1000) / 1000
+    utils.info("get_ask_upper_bound")
+    utils.info(target)
+    return target
 
 def get_ask_lower_bound(reference_value):
-    return math.floor((reference_value + settings.ASK_LOWER_BOUND_PROFIT_MARGIN) * 1000) / 1000
+    utils.info("get_ask_lower_bound")
+    utils.info(reference_value)
+    target = math.floor((reference_value + settings.ASK_LOWER_BOUND_PROFIT_MARGIN) * 1000) / 1000
+    utils.info(target)
+    utils.info("get_ask_lower_bound")
+    return target
 
 def get_ask_stop_loss(reference_value):
-    return math.floor((reference_value + settings.ASK_STOP_LOSS_MARGIN) * 1000) / 1000
+    utils.info("get_ask_stop_loss")
+    utils.info(reference_value)
+    target = math.floor((reference_value + settings.ASK_STOP_LOSS_MARGIN) * 1000) / 1000
+    utils.info(target)
+    utils.info("get_ask_stop_loss")
+    return target
 
 def get_bid_target_point(reference_value):
-    return math.floor((reference_value + settings.IFDOCO_BID_ENTRY_POINT) * 1000) / 1000
+    utils.info("get_bid_target_point")
+    utils.info(reference_value)
+    target = math.floor((reference_value + settings.IFDOCO_BID_ENTRY_POINT) * 1000) / 1000
+    utils.info(target)
+    utils.info("get_bid_target_point")
+    return target
 
 def get_bid_upper_bound(reference_value):
-    return math.floor((reference_value + settings.BID_UPPER_BOUND_PROFIT_MARGIN) * 1000) / 1000
+    utils.info("get_bid_upper_bound")
+    utils.info(reference_value)
+    target = math.floor((reference_value + settings.BID_UPPER_BOUND_PROFIT_MARGIN) * 1000) / 1000
+    utils.info(target)
+    utils.info("get_bid_upper_bound")
+    return target
 
 def get_bid_lower_bound(reference_value):
-    return math.floor((reference_value + settings.BID_LOWER_BOUND_PROFIT_MARGIN) * 1000) / 1000
+    utils.info("get_bid_lower_bound")
+    utils.info(reference_value)
+    target = math.floor((reference_value + settings.BID_LOWER_BOUND_PROFIT_MARGIN) * 1000) / 1000
+    utils.info(target)
+    utils.info("get_bid_lower_bound")
+    return target
 
 def get_bid_stop_loss(reference_value):
-    return math.floor((reference_value + settings.BID_STOP_LOSS_MARGIN) * 1000) / 1000
+    utils.info("get_bid_stop_loss")
+    utils.info(reference_value)
+    target = math.floor((reference_value + settings.BID_STOP_LOSS_MARGIN) * 1000) / 1000
+    utils.info(target)
+    utils.info("get_bid_stop_loss")
+    return target
