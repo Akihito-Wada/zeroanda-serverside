66c1f071e91271a237da5526e35ab753caf41068
wada <wada@strippers.jp>
2016-04-21 16:21:22 +0900

    nullhandler

--- zeroanda/settings/sandbox_settings.py
+++ zeroanda/settings/sandbox_settings.py
@@ -37,7 +37,7 @@ LOGGING = {
     'handlers': {
         'null': {
             'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
+            'class':'logging.NullHandler',
         },
         'console':{
             'level':'DEBUG',
