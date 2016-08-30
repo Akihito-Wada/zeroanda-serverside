4071bfb2ed41b2654ee5a238edd3d39f862927a5
wada <wada@strippers.jp>
2016-08-30 19:49:46 +0900

    implement Logger

--- zeroanda/zeroanda/classes/utils/loggerutils.py
+++ zeroanda/zeroanda/classes/utils/loggerutils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 import os
 
@@ -9,10 +10,34 @@ class Logger(object):
     @classmethod
     def setLogFileName(cls, targetname):
         file_log = logging.FileHandler(filename=os.path.join(settings.BASE_DIR, 'logs', 'trades', targetname + ".log"))
-        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
         file_log.setFormatter(formatter)
-        logging.getLogger().addHandler(file_log)
+        # logging.getLogger().addHandler(file_log)
+        logging.getLogger('trade').addHandler(file_log)
+
+    @classmethod
+    def setLogFileNameBySchedule(cls, scheduleModel):
+        # filename = "trade_" + str(scheduleModel.id) + "_" + scheduleModel.presentation_time.strftime("%Y%m%d_%H%M%S") + "_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = "trade_" + str(scheduleModel.id) + "_" + datetime.now().strftime("%Y%m%d_%H%M%S")
+        cls.setLogFileName(filename)
+
+    @classmethod
+    def debug(cls, value):
+        cls.logger.debug(value)
+
+    @classmethod
+    def warn(cls, value):
+        cls.logger.warn(value)
 
     @classmethod
     def info(cls, value):
         cls.logger.info(value)
+
+    @classmethod
+    def error(cls, value):
+        cls.logger.error(value)
+
+    @classmethod
+    def fatal(cls, value):
+        cls.logger.fatal(value)
+
