bde84cfbbbcaf04166cf70251e5fb30e1cb49655
wada <wada@strippers.jp>
2016-09-08 18:21:34 +0900

    modify database info.

--- zeroanda/settings/aws-i-4c8680d3_practice_settings.py
+++ zeroanda/settings/aws-i-4c8680d3_practice_settings.py
@@ -16,8 +16,6 @@ TOKEN = "8713400a434b3f4cfd2e2f9580da45ed-41a3452617aa84f441be90ca6ab0fc55"
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'dev_reversid'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dev_zeroanda',
         'USER': 'wadaakihito',
