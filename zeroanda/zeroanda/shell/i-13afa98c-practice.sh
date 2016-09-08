9c8e6c1164fdb5c797805da259fe53fcf394684c
wada <wada@strippers.jp>
2016-09-08 19:04:26 +0900

    rename aws.practice.settings

---
+++ zeroanda/zeroanda/shell/i-13afa98c-practice.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DJANGO_SETTINGS_MODULE=settings.aws-i-13afa98c_practice_settings
+. /home/akwada/Env/zeroanda/bin/activate
+cd /home/project/zeroanda-serverside/zeroanda
+python manage.py execute_process
