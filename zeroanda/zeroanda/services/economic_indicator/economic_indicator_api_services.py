f0efd6807b0f5354b1cadaa56ebc5cdbc2ecbcca
wada <wada@strippers.jp>
2016-09-12 16:30:31 +0900

    refacotring

--- zeroanda/zeroanda/services/economic_indicator/economic_indicator_api_services.py
+++ zeroanda/zeroanda/services/economic_indicator/economic_indicator_api_services.py
@@ -6,7 +6,7 @@ from dateutil.relativedelta import relativedelta
 
 from zeroanda import utils
 from  zeroanda.classes.net.http import Http
-from zeroanda.services.economic_indicator import EconomyIndicationValueObject
+from zeroanda.services.economic_indicator.vo.economy_indication_value_object import EconomyIndicationValueObject
 
 
 class EconomicIndicatorApiServiceFactory:
@@ -15,7 +15,7 @@ class EconomicIndicatorApiServiceFactory:
         return DailyFXService()
 
 class HttpService:
-    url = ""
+    _url = ""
 
     def get_economic_indicator(self, url):
         if url is None:
@@ -24,23 +24,19 @@ class HttpService:
         return http.get_csv_file(url)
 
 class DailyFXService(HttpService):
-    url = "http://www.dailyfx.com"
+    _url = "http://www.dailyfx.com"
 
     def get_latest_economic_indicator(self):
-        target_url = self.url + "/files/Calendar-09-11-2016.csv"
         year = datetime.now().year
         month = datetime.now().month
-        day = datetime.now().day
+        # day = datetime.now().day
+        day = 11
 
-        # target_date = self.get_next_sunday_datetime(year, 12, 26)
         target_date = self.__get_next_sunday_datetime(year, month, day)
-        unique_id = "{month}-{day}-{year}".format(month=str("{0:02d}".format(target_date.month)), day=str("{0:02d}".format(target_date.day)), year=str(target_date.year))\
-        # str("{0:02d}".format(target_date.month)) + "-" + str("{0:02d}".format(target_date.day)) + "-" + str(target_date.year)
-        # target_url = self.url + "/files/Calendar-" + str("{0:02d}".format(target_date.month)) + "-" + str("{0:02d}".format(target_date.day)) + "-" + str(target_date.year) + ".csv"
-        target_url = "{url}/files/Calendar-{unique_id}.csv".format(url=self._url, unique_id=unique_id)
-        utils.info(target_url)
-        result = self.get_economic_indicator(target_url)
 
+        unique_id = "{month}-{day}-{year}".format(month=str("{0:02d}".format(target_date.month)), day=str("{0:02d}".format(target_date.day)), year=str(target_date.year))
+        target_url = "{url}/files/Calendar-{unique_id}.csv".format(url=self._url, unique_id=unique_id)
+        result = self.get_economic_indicator(target_url)
         decoded_content = result.content.decode('utf-8')
         cr = csv.reader(decoded_content.splitlines(), delimiter=',')
         _tmp_list = list(cr)
@@ -49,10 +45,10 @@ class DailyFXService(HttpService):
             if index == 0:
                 continue
             vo = EconomyIndicationValueObject(row, target_date)
-            utils.info(vo)
+            # utils.info(vo)
             _list.append(vo)
         dto = EconomicIndicatorDTO(unique_id, vo)
-        return _list
+        return dto
 
     def __get_next_sunday_datetime(self, year, month, day):
         origin_date = datetime(year=year, month=month, day=day)
