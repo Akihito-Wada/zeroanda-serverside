36df0dd193659a171b55cb0a4177dcffe2e768f7
wada <wada@strippers.jp>
2016-09-13 17:22:08 +0900

    change the specification of get_schedule to get highest record.

--- zeroanda/zeroanda/controller/action.py
+++ zeroanda/zeroanda/controller/action.py
@@ -9,12 +9,13 @@ class Action:
     def __init__(self):
         self.__proxy = ScheduleProxyModel()
     def WatchSchedule(self):
-        schedules = self.__proxy.get_schedule()
-        utils.info(len(schedules))
-        if len(schedules) > 0:
-            Logger.setLogFileNameBySchedule(schedules[0])
-            self.__proxy.update_status_proceed(schedules[0])
-            self.startTickTack(schedules[0])
+        schedule = self.__proxy.get_schedule()
+        utils.info(schedule)
+        # if len(schedules) > 0:
+        if schedule != None:
+            Logger.setLogFileNameBySchedule(schedule)
+            self.__proxy.update_status_proceed(schedule)
+            self.startTickTack(schedule)
 
     def startTickTack(self, schedule):
         # MailManager.send_opening_mail(schedule)
