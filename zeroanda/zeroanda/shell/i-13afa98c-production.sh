60ae797cb8f75bbe6e00659da299be2de304eb35
Akihito Wada <akihitowada@Akihitos-MacBook-Air.local>
2016-09-22 17:11:13 +0900

    merge production.sh with practice

--- zeroanda/zeroanda/shell/i-13afa98c-production.sh
+++ zeroanda/zeroanda/shell/i-13afa98c-production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=settings.aws-i-13afa98c_production_settings
 . /home/akwada/Env/zeroanda/bin/activate
-cd /home/project/zeroanda/serverside/production
+cd /home/project/zeroanda/server/production
 python manage.py execute_process
