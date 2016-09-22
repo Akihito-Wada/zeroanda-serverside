2d1a449b44927f833921b678ec6295f9b79ccc77
wada <wada@strippers.jp>
2016-09-22 15:56:42 +0900

    change home-path

--- zeroanda/zeroanda/shell/i-13afa98c-practice.sh
+++ zeroanda/zeroanda/shell/i-13afa98c-practice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=settings.aws-i-13afa98c_practice_settings
 . /home/akwada/Env/zeroanda/bin/activate
-cd /home/project/zeroanda-serverside/zeroanda
+cd /home/project/zeroanda/serverside/practice
 python manage.py execute_process
