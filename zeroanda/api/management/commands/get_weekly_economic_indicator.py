41b6b86db8b78c6e8ff1e52c8a8ff377d4917baf
wada <wada@strippers.jp>
2016-10-04 14:32:05 +0900

    add new cron that get economic-indicator weekly.

---
+++ zeroanda/api/management/commands/get_weekly_economic_indicator.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from zeroanda.controller.action import Action
+import logging
+
+logger =logging.getLogger("django")
+
+class Command(BaseCommand):
+    help = 'get economic-indicator process.'
+    def handle(self, *args, **options):
+        action = Action()
+        action.WatchSchedule()
---
+++ zeroanda/zeroanda/shell/i-13afa98c-economic-indicator-practice.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DJANGO_SETTINGS_MODULE=settings.aws-i-13afa98c_practice_settings
+. /home/akwada/Env/zeroanda/bin/activate
+cd /home/project/zeroanda/server/practice/zeroanda
+python manage.py get_weekly_economic_indicator
